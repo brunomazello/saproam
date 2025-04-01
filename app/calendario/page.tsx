1ecaa375c0f38527da9c96252e6992d66eceb559
brunomazello <bruno.rmazello@gmail.com>
2025-04-01 02:08:58 -0300

    Adicionado timer para jogos

---
+++ app/calendario/page.tsx
@@ -0,0 +1,23 @@
+"use client";
+import logo from "../assets/logo.png";
+import {
+  LandPlot,
+  CircleGauge,
+  User,
+  ShieldQuestion,
+  ShieldAlert,
+} from "lucide-react";
+import { LinkButton } from "../components/button";
+import Image from "next/image";
+import AdComponent from "../components/AdComponent";
+import ListarTimes from "../components/listartimes";
+import ExibirTimes from "../components/exibirtimes";
+import FullCalendar from "@fullcalendar/react";
+import Calendario from "../components/fullcalendar";
+export default function Home() {
+  return (
+    <div className="min-h-dvh flex justify-center gap-8 flex-col md:mb-12">
+      <Calendario />
+    </div>
+  );
+}
